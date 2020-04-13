f70460ce8ce1e8cd1fe57801adf67d391e1c3d10
IgorIosifov <igoriosifov@yahoo.com>
2020-04-13 22:19:08 +0300

    Lesson 42. Refactoring: add redux

--- src/redux/dialogs-reducer.jsx
+++ src/redux/dialogs-reducer.jsx
@@ -1,7 +1,26 @@
 let ADD_MESSAGE = 'ADD-MESSAGE';
 let UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+    ],
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Asd1'},
+        {id: 4, message: 'Asd2'},
+        {id: 5, message: 'Asd3'}
+    ],
+    newMessage: ''
+};
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_MESSAGE:
             let newMessage = {
