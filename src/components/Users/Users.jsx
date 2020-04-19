6185cd94d27c5367fbeecdef3461e3cc18737ea6
IgorIosifov <igoriosifov@yahoo.com>
2020-04-19 18:22:14 +0300

    Lesson 51. Ajax users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,28 @@
 import React from "react";
 import s from './Users.module.css';
+import * as axios from "axios";
+import emptyUser from '../../assets/images/emptyUser.png';
+
 
 const Users = (props) => {
-
-   if (props.users.length === 0) {
-       props.setUsers([
-           {
-               id: 1,
-               avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBFC8gdBRergzgi31EKqSTlKhWUgqRwbxAtJKG9UX_iLOCVHFY&usqp=CAU',
-               followed: false,
-               fullName: 'Igor I.',
-               status: 'OK',
-               location: {city: 'Vlasikha', country: 'Russia'}
-           },
-           {
-               id: 2,
-               avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBFC8gdBRergzgi31EKqSTlKhWUgqRwbxAtJKG9UX_iLOCVHFY&usqp=CAU',
-               followed: true,
-               fullName: 'Timur G.',
-               status: 'OKOK',
-               location: {city: 'Moscow', country: 'Russia'}
-           },
-           {
-               id: 3,
-               avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBFC8gdBRergzgi31EKqSTlKhWUgqRwbxAtJKG9UX_iLOCVHFY&usqp=CAU',
-               followed: false,
-               fullName: 'Andrey S.',
-               status: 'OKOkOk',
-               location: {city: 'Iron', country: 'Russia'}
-           }]);
-   }
+    if (props.users.length === 0) {
+        axios.get("https://equipment-rest.herokuapp.com/users").then(response => props.setUsers(response.data))
+    }
 
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <div>
-                    <img src={u.avatar} className={s.avatar}/>
+                    <img src={u.avatar !=='' ? u.avatar : emptyUser} className={s.avatar}/>
                 </div>
                 <div>
                     {u.followed
-                        ? <button onClick={()=> {props.unfollow(u.id)}}>Unfollow</button>
-                        : <button onClick={()=> {props.follow(u.id)}}>Follow</button>}
+                        ? <button onClick={() => {
+                            props.unfollow(u.id)
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            props.follow(u.id)
+                        }}>Follow</button>}
                 </div>
                 <div>{u.fullName}</div>
                 <div>{u.status}</div>
